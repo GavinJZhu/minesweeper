519df9fc288176a5467c43ce172f18a8645ce04f
GavinJZhu <102933876+GavinJZhu@users.noreply.github.com>
2024-10-26 11:48:18 -0700

    2d arrays

--- src/GridPanel.java
+++ src/GridPanel.java
@@ -11,35 +11,38 @@ public class GridPanel extends JPanel implements ActionListener {
     ScoringPanel m_scoringPanel;
     ReplayPanel m_replayPanel;
     Minesweeper m_minesweeper;
+    Boolean hasBomb;
     int rows = 16;
     int columns = 16;
+    JButton[][] array = new JButton[rows][columns];
     JButton m_button;
-    GridPanel(){
+
+    GridPanel() {
         //add(new JButton("grid"));
         setup();
     }
-    void setup(){
-        GridLayout grid = new GridLayout(16,16);
+
+    void setup() {
+        GridLayout grid = new GridLayout(16, 16);
         setLayout(grid);
         setupButtons();
     }
-    void setupButtons(){
-        JButton[][] array = new JButton[rows][columns];
 
+    void setupButtons() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                 JButton button = new JButton();
-                 button.setIcon(changeButtonIcon(8));
-                 button.setName("test");
-                 this.add(button);
-                 button.addActionListener(this);
-                 array[i][j] = button;
+                JButton button = new JButton();
+                button.setIcon(changeButtonIcon(8));
+                button.setActionCommand(i + ", " + j);
+                this.add(button);
+                button.addActionListener(this);
+                array[i][j] = button;
             }
         }
-
     }
-    void setupButtons0(){
-        for (int i=0; i<256; i++){
+
+    void setupButtons0() {
+        for (int i = 0; i < 256; i++) {
             //String buttonNumber = Integer.toString(i);
             m_button = new JButton();
             //button.setPreferredSize(new Dimension(5,5));
@@ -47,15 +50,18 @@ public class GridPanel extends JPanel implements ActionListener {
             this.add(m_button);
         }
     }
-    public void restart(){
+
+    public void restart() {
         System.out.println("if won then communicate win with replay panel");
     }
-    public void setGamePanel(Minesweeper minesweeper){
-       m_minesweeper = minesweeper;
+
+    public void setGamePanel(Minesweeper minesweeper) {
+        m_minesweeper = minesweeper;
     }
+
     public ImageIcon changeButtonIcon(int surroundingBombs) {
-        String iconFileName = "bomb"+surroundingBombs;
-        URL imageURL = getClass().getResource(iconFileName+".png");
+        String iconFileName = "bomb" + surroundingBombs;
+        URL imageURL = getClass().getResource(iconFileName + ".png");
         BufferedImage image = null;
         try {
             image = ImageIO.read(imageURL);
@@ -68,9 +74,9 @@ public class GridPanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("debug GridPanel");
-        m_minesweeper.restart();
+        System.out.println(e.getActionCommand());
 //      tbd: figure out how to identify what button was selected
 
     }
+
 }
